ccd686526a7ab060be283fb929f0e842c0e95de2
MonsterTTL <1908097012@qq.com>
2023-03-18 20:34:05 +0800

    安全性检查和类型检查

--- KotlinBasic/src/main/kotlin/main.kt
+++ KotlinBasic/src/main/kotlin/main.kt
@@ -1,9 +1,19 @@
 fun main(){
-        ///test()
-        ///println(message("jack","kotlin"))
-        //lambdaTest(arr=arrayListOf(2,2,3,5,4))()
-        greet("wb",1)
+
+        // 类型安全检查 and Elvis运算符
+        val result:String? = nikeName(null)
+        val result2:String? = nickName("XuMingYang")
+        println("result1: $result, result2: $result2")
+        // 类型检查和转换
+        val greet:Any = "subString"
+        val Dog:Any = Animal()
+        val Cat:Any = Animal()
+        val ans = Dog == Cat
+        val ans2 = Dog == greet
+        println("Cat and Dog:${Dog == Cat};greet is Dog:${Dog == greet}")
+
 }
+
 //字符串模版 用${}定位
 fun message(name:String,mode:String):String = "Hello $name,Welcome to $mode"
 fun test(st1:String){
@@ -23,3 +33,32 @@ fun greet(name: String = "world", times: Int, message: String = "Hello") {
                 println("$message, $name!")
         }
 }
+
+fun maptest(){
+        val myMap = mapOf("1" to "One","2" to "Two","3" to "Three")
+
+        for(entr in myMap){
+                println("${entr.key}--->${entr.value}")
+        }
+}
+
+fun nikeName(name:String?):String?{
+        //可空类型
+        if(name == "William"){
+                return "Bill"
+        }
+        return name?.reversed()?.toUpperCase()?:"Joker" //安全运算符 和 Elvis运算符
+}
+
+fun nickName(name:String?) = when(name) {
+        "William" -> "Bill"
+        null -> "Joker"
+        else -> name.reversed().toUpperCase()
+}
+
+class Animal{
+        override fun equals(other: Any?): Boolean {
+                return other is Animal  //is 运算符，检查引用所指向对象是否属于特定类型 像InstanceOf
+        }
+}
+
